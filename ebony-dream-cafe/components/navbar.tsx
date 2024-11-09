41dccfd0acd4ed458592d7f55b34a79e4bb976b4
Ahmed <159935268+Solace-Code@users.noreply.github.com>
2024-11-09 03:21:54 -0800

    updated navbar component from a to link

--- ebony-dream-cafe/components/navbar.tsx
+++ ebony-dream-cafe/components/navbar.tsx
@@ -1,16 +1,25 @@
-// components/Navbar.tsx;
+// components/Navbar.tsx
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
-   <div className='navbar-container'>
-    <div className='navbar-items'>
-      <a href='/'>Home</a>
-      <a href='/#about'>About US</a>
-      <a href='/#about-2'>What we Offer</a>
-      <a href='/member'>MemberShip</a>
+    <div className='navbar-container'>
+      <div className='navbar-items'>
+        <Link href='/' passHref>
+          <span className='navbar-link'>Home</span>
+        </Link>
+        <Link href='/#about' passHref>
+          <span className='navbar-link'>About Us</span>
+        </Link>
+        <Link href='/#about-2' passHref>
+          <span className='navbar-link'>What we Offer</span>
+        </Link>
+        <Link href='/member' passHref>
+          <span className='navbar-link'>Membership</span>
+        </Link>
+      </div>
     </div>
-   </div>
   );
 };
 
